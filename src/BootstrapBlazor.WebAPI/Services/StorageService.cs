b8e1dffc4ba7da27d6560bdee504568b1fdbfad7
Alex chow <zhouchuanglin@gmail.com>
2023-07-09 02:02:14 +0800

    读取不用指定类型

--- src/BootstrapBlazor.WebAPI/Services/StorageService.cs
+++ src/BootstrapBlazor.WebAPI/Services/StorageService.cs
@@ -1,38 +1,64 @@
 ﻿using Microsoft.JSInterop;
+using System;
+using System.ComponentModel;
 
 namespace BootstrapBlazor.WebAPI.Services;
 
 public interface IStorage
 {
     public Task RemoveValue(string key);
-    public Task SetValue<TValue>(string key, TValue value) where TValue : class;
-    public Task<TValue?> GetValue<TValue>(string key, TValue? def = null) where TValue : class;
+    public Task SetValue<TValue>(string key, TValue value);
+    public Task<TValue?> GetValue<TValue>(string key, TValue? def);
 }
 
 public class StorageService : IStorage
 {
-    readonly IJSRuntime JSRuntime; 
+    readonly IJSRuntime JSRuntime;
 
     public StorageService(IJSRuntime jsRuntime)
     {
-        JSRuntime = jsRuntime; 
+        JSRuntime = jsRuntime;
     }
 
-    public async Task SetValue<TValue>(string key, TValue value) where TValue : class
+    public async Task SetValue<TValue>(string key, TValue value)
     {
         await JSRuntime.InvokeVoidAsync("eval", $"localStorage.setItem('{key}', '{value}')");
     }
 
-    public async Task<TValue?> GetValue<TValue>(string key, TValue? def) where TValue : class
+    public async Task<TValue?> GetValue<TValue>(string key, TValue? def)
     {
-        var cValue = await JSRuntime.InvokeAsync<TValue>("eval", $"localStorage.getItem('{key}');"); 
-        return cValue??def;
+        try
+        {
+            var cValue = await JSRuntime.InvokeAsync<TValue>("eval", $"localStorage.getItem('{key}');");
+            return cValue ?? def;
+        }
+        catch 
+        {
+            var cValue = await JSRuntime.InvokeAsync<string>("eval", $"localStorage.getItem('{key}');");
+            if ( cValue ==null )
+                return def;
+
+            var newValue = GetValueI<TValue>(cValue);
+            return newValue ?? def;
+            
+        }
     }
+    public static T? GetValueI<T>(string value)
+    {
+        TypeConverter converter = TypeDescriptor.GetConverter(typeof(T));
+        if (converter != null)
+        {
+            return (T?)converter.ConvertFrom(value);
+        }
+        return default;
+        //return (T)Convert.ChangeType(value, typeof(T));
+    }
+
     public async Task RemoveValue(string key)
     {
-         await JSRuntime.InvokeVoidAsync("eval", $"localStorage.removeItem('{key}')");
+        await JSRuntime.InvokeVoidAsync("eval", $"localStorage.removeItem('{key}')");
     }
-     
+ 
 
 }
 
