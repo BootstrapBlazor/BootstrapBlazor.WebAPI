eb5bf9fe9e84c5dbec4cccdc6f84b64fc2112dcd
Alex chow <zhouchuanglin@gmail.com>
2023-09-10 20:55:06 +0200

    Update WebSerialOptions.cs

--- src/BootstrapBlazor.WebAPI/WebSerialOptions.cs
+++ src/BootstrapBlazor.WebAPI/WebSerialOptions.cs
@@ -4,9 +4,7 @@
 // e-mail:zhouchuanglin@gmail.com 
 // **********************************
 
-using Microsoft.AspNetCore.Components;
 using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
 using System.Text.Json.Serialization;
 
 namespace BootstrapBlazor.Components;
@@ -213,19 +211,22 @@ public class WebSerialSignalsSetting
 {
     /// <summary>
     /// 中断
+    /// <para></para>如果 Break 为 true，则表示已中断。如果 Break 为 false，则表示未中断。
     /// </summary>
     [DisplayName("中断")]
     public bool? Break { get; set; }
 
     /// <summary>
-    /// 数据终端就绪
+    /// 数据终端准备就绪 DTR（Data Terminal Ready）
+    /// <para></para>如果 DTR 为 true，则表示已准备好接收数据。如果 DTR 为 false，则表示未准备好接收数据。Pin 4
     /// </summary>
     [JsonPropertyName("DTR")]
     [DisplayName("DTR")]
     public bool? DTR { get; set; }
 
     /// <summary>
-    /// 请求发送
+    /// 请求发送 RTS（Request To Send）
+    /// <para></para>如果 RTS 为 true，则表示已准备好发送数据。如果 RTS 为 false，则表示未准备好发送数据。Pin 7
     /// </summary>
     [JsonPropertyName("RTS")]
     [DisplayName("RTS")]
@@ -233,27 +234,43 @@ public class WebSerialSignalsSetting
 
 }
 
+//RS-232C接口定义(DB9)
+//引脚 定义 符号
+//1 载波检测 DCD（Data Carrier Detect）
+//2 接收数据 RXD（Received Data）
+//3 发送数据 TXD（Transmit Data）
+//4 数据终端准备就绪 DTR（Data Terminal Ready）
+//5 信号地 SG（Signal Ground）
+//6 数据准备就绪 DSR（Data Set Ready）
+//7 请求发送 RTS（Request To Send）
+//8 清除发送 CTS（Clear To Send）
+//9 振铃提示 RI（Ring Indicator）
+
 public class WebSerialSignals
 {
     /// <summary>
-    /// Ring Indicator
-    /// </summary>
+    /// 振铃提示 RI（Ring Indicator）
+    /// <para></para>如果 RI 为 true，则表示已检测到振铃。如果 RI 为 false，则表示未检测到振铃。Pin 9
+    /// </summary> 
     public bool RING { get; set; }
 
     /// <summary>
-    /// Data Set Ready
+    /// 数据准备就绪 DSR（Data Set Ready）
+    /// <para></para>如果 DSR 为 true，则表示已准备好接收数据。如果 DSR 为 false，则表示未准备好接收数据。Pin 6
     /// </summary>
     public bool DSR { get; set; }
 
 
     /// <summary>
-    /// Clear To Send
+    /// 清除发送 CTS（Clear To Send）
+    /// <para></para>如果 CTS 为 true，则表示已准备好发送数据。如果 CTS 为 false，则表示未准备好发送数据。Pin 8
     /// </summary>
     public bool CTS { get; set; }
 
 
     /// <summary>
-    /// Data Carrier Detect
+    /// 载波检测 DCD（Data Carrier Detect）
+    /// <para></para>如果 DCD 为 true，则表示已检测到载波。如果 DCD 为 false，则表示未检测到载波。Pin 1
     /// </summary>
     public bool DCD { get; set; }
 
